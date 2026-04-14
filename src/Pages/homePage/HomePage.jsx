be3fcdeb2b7b77cb5e12efe4aa8fc3f2acd836f6
Miskat-BD <mishkatahmed321@gmail.comgit config --global user.name Miskat-BDgit commit -m first>
2026-04-14 11:42:25 +0600

    tailwind and daisyUI connceted

--- src/Pages/homePage/HomePage.jsx
+++ src/Pages/homePage/HomePage.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const HomePage = () => {
     return (
         <div>
-            <h2>Home page ekhane</h2>
+            <h2 className='text-red-400'>Home page ekhane</h2>
         </div>
     );
 };
--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router';
+import Navbar from '../components/Navbar/Navbar';
 
 const MainLayout = () => {
     return (
         <div>
-            <h1>Navbar</h1>
+            <Navbar/>
             <Outlet></Outlet>
         </div>
     );
