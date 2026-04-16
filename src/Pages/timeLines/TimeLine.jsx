6b200b9c57aafec59d44ef83e3d79ecafa262cd2
Miskat-BD <mishkatahmed321@gmail.comgit config --global user.name Miskat-BDgit commit -m first>
2026-04-16 12:15:05 +0600

    timeline filter section fixed

--- src/Pages/timeLines/TimeLine.jsx
+++ src/Pages/timeLines/TimeLine.jsx
@@ -8,34 +8,30 @@ const TimeLine = () => {
     const [filterTimeline, setFilterTimeline] = useState([...timeline])
     // console.log(timeline, 'timeline');
     const handleFilter = (click) => {
-        if(click == 'All'){
+        if (click == 'All') {
             setFilterTimeline(timeline)
             return;
         }
         const newItem = timeline.filter(item => item.type == click)
         // console.log(newItem);
         setFilterTimeline(newItem)
-        
+
     }
     return (
         <div className="container mx-auto my-10">
             <div className='text-5xl font-bold text-[#1F2937] mb-7'>
                 <h2>Timeline</h2>
             </div>
-           
-            <div className="dropdown dropdown-start my-4">
-                <div tabIndex={0} role="button" className="btn m-1">All ⬇️</div>
-                <ul tabIndex="-1" className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
-                    <li onClick={() => handleFilter('All')}><a>All</a></li>
-                    <li onClick={() => handleFilter('Call')}><a>Call</a></li>
-                    <li onClick={() => handleFilter('Text')}><a>Text</a></li>
-                    <li onClick={() => handleFilter('Video')}><a>Video</a></li>
-                    
-                </ul>
-            </div>
+            <select defaultValue="All" className="select mb-7 ml-3">
+                
+                <option onClick={() => handleFilter('All')}>All</option>
+                <option onClick={() => handleFilter('Call')}>Call</option>
+                <option onClick={() => handleFilter('Text')}>Text</option>
+                <option onClick={() => handleFilter('Video')}>Video</option>
+            </select>
             {
                 filterTimeline.length == 0 ? (
-                    <EmptyTimeline text={'No Activity Yet'}/>
+                    <EmptyTimeline text={'No Activity Yet'} />
                 ) :
                     (filterTimeline.map((card, i) => <TimelinesCard key={i} card={card}></TimelinesCard>))
             }
